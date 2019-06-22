092fd418226c5845fda53a4d311adc8210db5d03
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2019-06-22 12:12:13 +0530

    student chocies; data update

---
+++ server/src/StudentChoices.scala
@@ -0,0 +1,49 @@
+package courses 
+
+import StudentChoices._
+import scala.util.Try
+
+object StudentChoices{
+    val groupMap : Map[String, Set[Course]] = 
+        Map("Undergraduate 3rd year" -> CourseData.core1.toSet,
+            "Int. Ph. D second year" -> CourseData.core2.toSet)
+    def groupCourses(gp: String) : Set[Course] = groupMap.getOrElse(gp, Set())
+
+    def getCourse(s: String) = CourseData.get(s.trim.take(6))
+
+    def fromVec(v: Vector[String]) = 
+        StudentChoices(
+            v(1),
+            v(2),
+            v(3).split(",").toSet.map(getCourse).flatten,
+            Try(v(4).split(",").toSet.map(getCourse).flatten).getOrElse(Set())
+        )
+    
+    lazy val dataLines = os.read.lines(Server.dat / "students.tsv").map(_.split("\t").toVector).toVector.drop(1)
+
+    lazy val all = dataLines.map(fromVec(_))
+
+    def strongClashes(sch: Schedule) = 
+        sch.clashes.map{
+            case (c1, c2) => (c1, c2) -> all.filter(_.strongClash(c1, c2))
+        }.toMap
+
+    def numStrongClashes(sch: Schedule) = strongClashes(sch).mapValues(_.size)
+
+    def totalStrongClashes(sch: Schedule) = numStrongClashes(sch).values.sum
+
+    def weakClashes(sch: Schedule) = 
+        sch.clashes.map{
+        case (c1, c2) => (c1, c2) -> all.filter(_.strongClash(c1, c2))
+    }.toMap
+    
+}
+
+case class StudentChoices(name: String, group: String, willTake: Set[Course], mayTake: Set[Course]){
+    val strongTake = willTake.union(groupCourses(group))
+    val weakTake = strongTake.union(mayTake)
+
+    def strongClash(c1: Course, c2: Course) = strongTake.contains(c1) && strongTake.contains(c2)
+
+    def weakClash(c1: Course, c2: Course) = weakTake.contains(c1) && weakTake.contains(c2)
+}
